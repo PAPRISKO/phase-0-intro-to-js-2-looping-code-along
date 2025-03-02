0ec6afcabd971be9c0af94bef94563de8eb47fe1
paprisko <jackmchussein965@gmail.com>
2025-03-02 14:44:11 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// Function to generate thank you cards
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function for counting down from a given number to 0
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Examples in use:
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+  countDown(10);
+  
